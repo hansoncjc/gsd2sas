32f86bed0f9009c53000b67643391e59f6d205bb
hansoncjc <hansonjc@uw.edu>
2025-05-14 13:42:33 -0700

    extract_position

---
+++ gsd2sas/io.py
@@ -0,0 +1,41 @@
+import numpy as np
+from gsd import hoomd
+
+def extract_positions(gsd_file, output_file):
+    """
+    Load particle positions from a GSD file. 
+
+    Parameters
+    ----------
+    gsd_file : str
+        Path to the GSD file.
+    out_file : str
+        Path to the output text file.
+    Returns
+    -------
+    positions : np.ndarray
+        Particle positions. Shape:
+        - (N, D) if single frame
+        - (F, N, D) if all frames
+    """
+    # Open gsd file
+    traj = hoomd.open(name=gsd_file, mode='r')
+    
+    # Save file if 
+    if output_file is not None:
+        with open(output_file, 'w') as f:
+        
+            # Loop through frames
+            for frame in traj:
+                
+                # Read data
+                N = frame.particles.N
+                box = frame.configuration.box
+                x = frame.particles.position
+                
+                # Write to text file
+                f.write('{:d}\n'.format(N))
+                f.write('{:.5f} {:.5f} {:.5f}\n'.format(box[0], box[1], box[2]))
+                for pos in x:
+                    f.write('{:.5f} {:.5f} {:.5f}\n'.format(pos[0], pos[1], pos[2]))
+                f.write('\n')
